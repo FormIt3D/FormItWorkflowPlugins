9214bdf66f2b91d8ceafbffb6cf559d1a36da2dd
Josh Goldstein <jdeangoldstein@gmail.com>
2018-08-16 16:12:08 -0600

    Adding ability to extract bump and cutout maps too

--- ExtractMaterialTextures/ExtractMaterialTextures_Client.js
+++ ExtractMaterialTextures/ExtractMaterialTextures_Client.js
@@ -21,6 +21,9 @@ deanstein.ExtractMaterialTextures = function(args)
         return undefined;
     }
 
+    // create an array to store the various texture maps
+    var bitmapDataArray = [];
+
     // if you're not in the Main History, calculate the depth to extract the correct history data
     var historyDepth = (currentSelection[0]["ids"].length) -1;
     console.log("Current history depth: " + historyDepth);
@@ -36,29 +39,90 @@ deanstein.ExtractMaterialTextures = function(args)
     if (selectionMaterialID === 4294967295)
     {
         console.log("This geometry has no Material applied to it.");
-        return undefined;
+        // notifications aren't yet supported in FormIt v16.x
+        //FormIt.UI.ShowNotification("This geometry has no Material applied to it.", FormIt.NotificationType.Warning, 0);
+        bitmapDataArray.push("undefined", "undefined", "undefined");
+        return bitmapDataArray;
     }
 
     // get the material data for the selection
     var selectionMaterialData = WSM.APIGetMaterialDataReadOnly(nHistoryID, selectionMaterialID);
     console.log("Current selection material data: " + JSON.stringify(selectionMaterialData));
 
+    // define how to get texture bitmap data given the history and texture ID
+    function getBitmapDataFromTextureID(nHistoryID, textureID) {
+
+       // get the texture data
+       var textureData = WSM.APIGetTextureDataReadOnly(nHistoryID, textureID);
+       //console.log("Current selection texture data: " + JSON.stringify(textureData));
+       var bitmapData = textureData.bitmap;
+       //console.log("bitmap data: " + JSON.stringify(bitmapData));
+       return bitmapData;
+    }
+
     // get the texture ID for the material
     var selectionTextureID = selectionMaterialData.nTextureID;
     console.log("Selection textureID: " + selectionTextureID);
-    // if the WSM Invalid ID is detected, throw an error and return undefined
-    if (selectionTextureID === 4294967295)
+
+    // get the texture data if it exists and push it into the array
+    if (selectionTextureID != 4294967295)
     {
-        console.log("This Material has no texture associated with it.");
-        return undefined;
+        console.log("Texture detected.");
+
+        var textureBitmapData = getBitmapDataFromTextureID(nHistoryID, selectionTextureID);
+        // push the texture data into the array
+        bitmapDataArray.push(textureBitmapData);
+        //console.log("bitmapDataArray: " + bitmapDataArray);
+
+    } else if (selectionTextureID === 4294967295) {
+
+        console.log("No texture detected.");
+        bitmapDataArray.push("undefined");
+        //console.log("bitmapDataArray: " + bitmapDataArray);
     }
 
-    // get the texture data
-    var textureData = WSM.APIGetTextureDataReadOnly(nHistoryID, selectionTextureID);
-    //console.log("Current selection texture data: " + JSON.stringify(textureData));
+    // check for additional maps
+    if (selectionMaterialData.aAdditionalTextures.length != 0) {
+        console.log("Additional images detected.");
 
-    var bitmapData = textureData.bitmap;
-    //console.log("bitmap data: " + JSON.stringify(bitmapData));
+        // need to check for the specific maps in order
+        if (selectionMaterialData.aAdditionalTextures[0].first === "normalMap") {
+            console.log("Normal map detected.");
 
-    return bitmapData;
+            var normalMapBitmapData = getBitmapDataFromTextureID(nHistoryID, selectionMaterialData.aAdditionalTextures[0].second);
+
+            bitmapDataArray.push(normalMapBitmapData);
+
+            if (selectionMaterialData.aAdditionalTextures[1]) {
+                console.log("Opacity map detected.");
+    
+                var opacityMapBitmapData = getBitmapDataFromTextureID(nHistoryID, selectionMaterialData.aAdditionalTextures[1].second);
+    
+                bitmapDataArray.push(opacityMapBitmapData);
+            } else {
+                console.log("No opacity map detected.");
+                bitmapDataArray.push("undefined");
+            }
+
+        } else {
+            console.log("No normal map detected.");
+            // add an undefined for the normal map
+            bitmapDataArray.push("undefined");
+
+            // if we get here, there must be no normal but an opacity map
+            console.log("Opacity map detected.");
+
+            var opacityMapBitmapData = getBitmapDataFromTextureID(nHistoryID, selectionMaterialData.aAdditionalTextures[0].second);
+
+            bitmapDataArray.push(opacityMapBitmapData);
+        }
+
+    } else {
+        console.log("No additional images detected.");
+        // add undefined for the missing maps
+        bitmapDataArray.push("undefined", "undefined");
+    }
+
+    //console.log(bitmapDataArray);
+    return bitmapDataArray;
 }
--- ExtractMaterialTextures/ExtractMaterialTextures_Web.js
+++ ExtractMaterialTextures/ExtractMaterialTextures_Web.js
@@ -3,15 +3,32 @@ if (typeof deanstein == 'undefined')
     deanstein = {};
 }
 
+// define all image types that can be associated with a Material
+var imgTypesArray = ["Texture","Bump","Cutout"];
+
 deanstein.PlaceholderImage = function()
 {
-    console.log("Creating placeholder image");
-    // create a placeholder image
-    var img = document.createElement('img');
-    img.id = 'image';
-    window.document.body.appendChild(img);
-    img.src = 'img/placeholder.png';
+    // create a placeholder image and label for each type
+    console.log("Creating placeholder images and labels");
+
+    function createPlaceholderHTMLElements(type) {
+        var label = document.createElement('p');
+        label.id = type + "Label";
+        label.className = "textureLabel";
+        var textNode = document.createTextNode(type + ":");
+        label.appendChild(textNode);
+        window.document.body.appendChild(label);
+
+        var img = document.createElement('img');
+        img.id = type + "Image";
+        window.document.body.appendChild(img);
+        img.src = 'img/placeholder.png';
+    }
+
+    imgTypesArray.forEach(createPlaceholderHTMLElements);
 };
+
+
 
 // Submit runs from the HTML page.  This script gets loaded up in both FormIt's
 // JS engine and also in the embedded web JS engine inside the panel.
@@ -20,29 +37,39 @@ deanstein.Submit = function()
     var args = {
     //"radius": parseFloat(document.a.radius.value),
     //"cleanup": document.a.cleanup.checked
-    }
+    };
 
-    console.log("deanstein.ExtractMaterialTextures");
-    console.log("args");
-    // NOTE: window.FormItInterface.CallMethod will call the function
-    // defined above with the given args.  This is needed to communicate
-    // between the web JS enging process and the FormIt process.
-    window.FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args);
-    FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args,
-        function(bitmapData)
-        {
-            var img = document.getElementById('image');
-            if (bitmapData === "undefined")
+    var updateImage = function(bitmapDataArray)
+    {
+        // this array turned into a string, so parse it back into an array
+        bitmapDataArray = JSON.parse(bitmapDataArray);
+        
+        // for each of the items in the array, update the associated HTML image
+        for (var i = 0; i < imgTypesArray.length; i++) {
+
+            console.log("Checking image for " + imgTypesArray[i]);
+            var img = document.getElementById(imgTypesArray[i] + "Image");
+            if ((bitmapDataArray[i] === "undefined") || bitmapDataArray === "undefined")
             {
-                img.src = 'img/notexture.png';
+                console.log("No image found for " + imgTypesArray[i]);
+                img.src = 'img/noImage.png';
             } else {
                 //debugger;
-                var evalBitmapData = eval(bitmapData);
+                console.log("Drawing new bitmap data for " + imgTypesArray[i]);
+                var evalBitmapData = eval(bitmapDataArray[i]);
                 var data = new Uint8Array(evalBitmapData);
                 var blob = new Blob([data], {type: 'image/bmp'});
                 var imgURL = URL.createObjectURL(blob);
                 img.src = imgURL;
             }
+        }
+    }
 
-        });
+    //console.log("deanstein.ExtractMaterialTextures");
+    //console.log("args");
+    // NOTE: window.FormItInterface.CallMethod will call the function
+    // defined above with the given args.  This is needed to communicate
+    // between the web JS enging process and the FormIt process.
+    window.FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args);
+    FormItInterface.CallMethod("deanstein.ExtractMaterialTextures", args, updateImage);
 }
