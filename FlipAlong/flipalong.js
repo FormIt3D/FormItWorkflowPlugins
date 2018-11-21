c11994968d98535b83a699a1591ffeaa2b69245f
Josh Goldstein <josh.goldstein@autodesk.com>
2018-11-20 20:21:29 -0700

    Make Flip Along work inside Groups, with multiple selections, and result in only one Undo state

--- FlipAlong/flipalong.js
+++ FlipAlong/flipalong.js
@@ -1,60 +1,109 @@
 FlipAlongPlugin = {};
+
 FlipAlongPlugin.ButtonX = function()
 {
-    //console.log("FlipAlongPlugin.ButtonX clicked!!");
+    FormIt.UndoManagement.BeginState();
+
     var selections = FormIt.Selection.GetSelections();
     //console.log("Selections: " + JSON.stringify(selections));
     var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     //console.log("AABB: " + JSON.stringify(AABB));
     
-    var histID = FormIt.Model.GetHistoryID();
-    //console.log("histID: " + histID);
+    var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
+    console.log("Current history: " + JSON.stringify(nHistoryID));
+
     var pt1 = WSM.Geom.Point3d(AABB.upper.x, AABB.lower.y, AABB.lower.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.lower.z); 
     var pt3 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.upper.z); 
-    //WSM.APICreateRectangle(histID, pt1, pt2, pt3, false);
-    
-    var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(AABB.upper.x * 2 + (AABB.upper.x - AABB.lower.x) * .1 , 0.0, 0.0) ,
-        WSM.Geom.Vector3d(-1, 0, 0), WSM.Geom.Vector3d(0, 1, 0), WSM.Geom.Vector3d(0, 0, 1));
-    
-    WSM.APICopyOrSketchAndTransformObjects( histID, histID, selections, trans, 1 /*one copy */, false /*bGroupBodies*/);
+
+    // for each object selected, execute the flip along
+    for (var j = 0; j < selections.length; j++)
+    {
+        // if you're not in the Main History, need to calculate the depth to extract the correct history data
+        var historyDepth = (selections[j]["ids"].length) -1;
+        console.log("Current history depth: " + historyDepth);        
+
+        // get objectID of the current selection
+        var nObjectID = selections[j]["ids"][historyDepth]["Object"];
+        console.log("Object ID: " + JSON.stringify(nObjectID));
+
+        var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(AABB.upper.x * 2 + (AABB.upper.x - AABB.lower.x) * .1 , 0.0, 0.0) ,
+            WSM.Geom.Vector3d(-1, 0, 0), WSM.Geom.Vector3d(0, 1, 0), WSM.Geom.Vector3d(0, 0, 1));
+
+        WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
+    }
+
+    FormIt.UndoManagement.EndState("Flip Along");
 }
 FormIt.Commands.RegisterJSCommand("FlipAlongPlugin.ButtonX");
 
 FlipAlongPlugin.ButtonY = function()
 {
-    //console.log("FlipAlongPlugin.ButtonY clicked!!");
+    FormIt.UndoManagement.BeginState();
+
     var selections = FormIt.Selection.GetSelections();
     var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     
-    var histID = FormIt.Model.GetHistoryID();
+    var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
+    console.log("Current history: " + JSON.stringify(nHistoryID));
+
     var pt1 = WSM.Geom.Point3d(AABB.lower.x, AABB.upper.y, AABB.lower.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.lower.z); 
     var pt3 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.upper.z); 
-    //WSM.APICreateRectangle(histID, pt1, pt2, pt3, false);
 
-    var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, AABB.upper.y * 2 + (AABB.upper.y - AABB.lower.y) * .1 , 0.0) ,
+    // for each object selected, execute the flip along
+    for (var j = 0; j < selections.length; j++)
+    {
+        // if you're not in the Main History, need to calculate the depth to extract the correct history data
+        var historyDepth = (selections[j]["ids"].length) -1;
+        console.log("Current history depth: " + historyDepth);        
+
+        // get objectID of the current selection
+        var nObjectID = selections[j]["ids"][historyDepth]["Object"];
+        console.log("Object ID: " + JSON.stringify(nObjectID));
+
+        var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, AABB.upper.y * 2 + (AABB.upper.y - AABB.lower.y) * .1 , 0.0) ,
         WSM.Geom.Vector3d(1, 0, 0), WSM.Geom.Vector3d(0, -1, 0), WSM.Geom.Vector3d(0, 0, 1));
     
-    WSM.APICopyOrSketchAndTransformObjects( histID, histID, selections, trans, 1 /*one copy */, false /*bGroupBodies*/);
+        WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
+    }
+
+    FormIt.UndoManagement.EndState("Flip Along");
 }
 FormIt.Commands.RegisterJSCommand("FlipAlongPlugin.ButtonY");
 
 FlipAlongPlugin.ButtonZ = function()
 {
-    //console.log("FlipAlongPlugin.ButtonZ clicked!!");
+    FormIt.UndoManagement.BeginState();
+
     var selections = FormIt.Selection.GetSelections();
     var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     
-    var histID = FormIt.Model.GetHistoryID();
+    var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
+    console.log("Current history: " + JSON.stringify(nHistoryID));
+
     var pt1 = WSM.Geom.Point3d(AABB.lower.x, AABB.lower.y, AABB.upper.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.lower.y, AABB.upper.z); 
     var pt3 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.upper.z); 
-    //WSM.APICreateRectangle(histID, pt1, pt2, pt3, false);
     
-    var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, 0.0, AABB.upper.z * 2 + (AABB.upper.z - AABB.lower.z) * .1) ,
+    // for each object selected, execute the flip along
+    for (var j = 0; j < selections.length; j++)
+    {
+        // if you're not in the Main History, need to calculate the depth to extract the correct history data
+        var historyDepth = (selections[j]["ids"].length) -1;
+        console.log("Current history depth: " + historyDepth);        
+
+        // get objectID of the current selection
+        var nObjectID = selections[j]["ids"][historyDepth]["Object"];
+        console.log("Object ID: " + JSON.stringify(nObjectID));
+
+            
+        var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, 0.0, AABB.upper.z * 2 + (AABB.upper.z - AABB.lower.z) * .1) ,
         WSM.Geom.Vector3d(1, 0, 0), WSM.Geom.Vector3d(0, 1, 0), WSM.Geom.Vector3d(0, 0, -1));
-    
-    WSM.APICopyOrSketchAndTransformObjects( histID, histID, selections, trans, 1 /*one copy */, false /*bGroupBodies*/);
+
+     WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
+    }
+
+    FormIt.UndoManagement.EndState("Flip Along");
 }
 FormIt.Commands.RegisterJSCommand("FlipAlongPlugin.ButtonZ");
