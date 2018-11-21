f9eecfe6358fbeb1614649ca629f2198d078a09d
Josh Goldstein <josh.goldstein@autodesk.com>
2018-11-20 21:28:50 -0700

    Updating FlipAlong comments and console output

--- FlipAlong/flipalong.js
+++ FlipAlong/flipalong.js
@@ -1,5 +1,7 @@
 FlipAlongPlugin = {};
 
+// TODO: figure out why combining these into a single function with a direction argument gets the LCS translation wrong
+
 FlipAlongPlugin.ButtonX = function()
 {
     FormIt.UndoManagement.BeginState();
@@ -10,7 +12,7 @@ FlipAlongPlugin.ButtonX = function()
     //console.log("AABB: " + JSON.stringify(AABB));
     
     var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
-    console.log("Current history: " + JSON.stringify(nHistoryID));
+    //console.log("Current history: " + JSON.stringify(nHistoryID));
 
     var pt1 = WSM.Geom.Point3d(AABB.upper.x, AABB.lower.y, AABB.lower.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.lower.z); 
@@ -21,19 +23,22 @@ FlipAlongPlugin.ButtonX = function()
     {
         // if you're not in the Main History, need to calculate the depth to extract the correct history data
         var historyDepth = (selections[j]["ids"].length) -1;
-        console.log("Current history depth: " + historyDepth);        
+        //console.log("Current history depth: " + historyDepth);        
 
         // get objectID of the current selection
         var nObjectID = selections[j]["ids"][historyDepth]["Object"];
-        console.log("Object ID: " + JSON.stringify(nObjectID));
+        //console.log("Object ID: " + JSON.stringify(nObjectID));
 
+        // define the translation
         var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(AABB.upper.x * 2 + (AABB.upper.x - AABB.lower.x) * .1 , 0.0, 0.0) ,
             WSM.Geom.Vector3d(-1, 0, 0), WSM.Geom.Vector3d(0, 1, 0), WSM.Geom.Vector3d(0, 0, 1));
 
+        // copy geometry using the given translation
         WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
     }
 
     FormIt.UndoManagement.EndState("Flip Along");
+    console.log("Flipped " + selections.length + " object(s) around the X axis in History " + nHistoryID);
 }
 FormIt.Commands.RegisterJSCommand("FlipAlongPlugin.ButtonX");
 
@@ -45,7 +50,7 @@ FlipAlongPlugin.ButtonY = function()
     var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     
     var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
-    console.log("Current history: " + JSON.stringify(nHistoryID));
+    //console.log("Current history: " + JSON.stringify(nHistoryID));
 
     var pt1 = WSM.Geom.Point3d(AABB.lower.x, AABB.upper.y, AABB.lower.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.upper.y, AABB.lower.z); 
@@ -56,16 +61,19 @@ FlipAlongPlugin.ButtonY = function()
     {
         // if you're not in the Main History, need to calculate the depth to extract the correct history data
         var historyDepth = (selections[j]["ids"].length) -1;
-        console.log("Current history depth: " + historyDepth);        
+        //console.log("Current history depth: " + historyDepth);        
 
         // get objectID of the current selection
         var nObjectID = selections[j]["ids"][historyDepth]["Object"];
-        console.log("Object ID: " + JSON.stringify(nObjectID));
+        //console.log("Object ID: " + JSON.stringify(nObjectID));
 
+        // define the translation
         var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, AABB.upper.y * 2 + (AABB.upper.y - AABB.lower.y) * .1 , 0.0) ,
         WSM.Geom.Vector3d(1, 0, 0), WSM.Geom.Vector3d(0, -1, 0), WSM.Geom.Vector3d(0, 0, 1));
     
+        // copy geometry using the given translation
         WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
+        console.log("Flipped " + selections.length + " object(s) around the Y axis in History " + nHistoryID);
     }
 
     FormIt.UndoManagement.EndState("Flip Along");
@@ -80,7 +88,7 @@ FlipAlongPlugin.ButtonZ = function()
     var AABB = WSM.Utils.GetAxisAlignedBoundingBox(selections, WSM.Utils.CoordSystem.HCS);
     
     var nHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
-    console.log("Current history: " + JSON.stringify(nHistoryID));
+    //console.log("Current history: " + JSON.stringify(nHistoryID));
 
     var pt1 = WSM.Geom.Point3d(AABB.lower.x, AABB.lower.y, AABB.upper.z); 
     var pt2 = WSM.Geom.Point3d(AABB.upper.x, AABB.lower.y, AABB.upper.z); 
@@ -91,19 +99,21 @@ FlipAlongPlugin.ButtonZ = function()
     {
         // if you're not in the Main History, need to calculate the depth to extract the correct history data
         var historyDepth = (selections[j]["ids"].length) -1;
-        console.log("Current history depth: " + historyDepth);        
+        //console.log("Current history depth: " + historyDepth);        
 
         // get objectID of the current selection
         var nObjectID = selections[j]["ids"][historyDepth]["Object"];
-        console.log("Object ID: " + JSON.stringify(nObjectID));
+        //console.log("Object ID: " + JSON.stringify(nObjectID));
 
-            
+        // define the translation
         var trans = WSM.Geom.MakeRigidTransform(WSM.Geom.Point3d(0.0, 0.0, AABB.upper.z * 2 + (AABB.upper.z - AABB.lower.z) * .1) ,
         WSM.Geom.Vector3d(1, 0, 0), WSM.Geom.Vector3d(0, 1, 0), WSM.Geom.Vector3d(0, 0, -1));
 
-     WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
+        // copy geometry using the given translation
+        WSM.APICopyOrSketchAndTransformObjects(nHistoryID, nHistoryID, nObjectID, trans, 1 /*one copy */, false /*bGroupBodies*/);
     }
 
     FormIt.UndoManagement.EndState("Flip Along");
+    console.log("Flipped " + selections.length + " object(s) around the Z axis in History " + nHistoryID);
 }
 FormIt.Commands.RegisterJSCommand("FlipAlongPlugin.ButtonZ");
