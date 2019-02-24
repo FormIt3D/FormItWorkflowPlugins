a00ee5838cc10665b587bb6560498c9e3e4ad893
Jeff Hauswirth <jeff.hauswirth@autodesk.com>
2019-02-23 17:07:22 -0700

    Switching from GlueToSurface to WSM.Tools.PlacementOrientation.Decal.  Adding  hole cutting components.

--- CutHole/cuthole.js
+++ CutHole/cuthole.js
@@ -1,15 +1,15 @@
 CutHoles = {};
 
-CutHoles.GlueToSurface = function()
+CutHoles.ApplyDecal = function()
 {
     var value = {};
-    value.GlueToSurface = true;
+    value.PlacementOrientation = WSM.Tools.PlacementOrientation.Decal;
     // Apply the FormIt::PlacementOptions attribute to the component
-    var currentHistory = FormIt.Model.GetHistoryID();
+    var currentHistory = FormIt.GroupEdit.GetEditingHistoryID();
     WSM.Utils.SetOrCreateStringAttributeForObject(currentHistory,
-        WSM.INVALID_ID, "FormIt::PlacementOptions", JSON.stringify(value));
+        WSM.INVALID_ID, "WSM::Tools::PlacementOrientation", JSON.stringify(value));
 }
-FormIt.Commands.RegisterJSCommand("CutHoles.GlueToSurface");
+FormIt.Commands.RegisterJSCommand("CutHoles.ApplyDecal");
 
 // MakeCuttingObject marks selection to be the cutting object(s) and sets the layer to CutHoles.CutHole and
 // applies the CutHoles.CutHole attribute.
@@ -27,7 +27,7 @@ CutHoles.MakeCuttingObject = function()
             var editingHistoryID = FormIt.GroupEdit.GetEditingHistoryID();
             // Apply the CutHoles.CutHole attribute to the instance's ref History
             WSM.Utils.SetOrCreateStringAttributeForObject(nRefHistoryID, WSM.INVALID_ID, "CutHoles.CutHole", "");
-            // TODO: Add FormIt::PlacementOptions GlueToSurface:true
+
             var allObjs = WSM.APIGetAllNonOwnedReadOnly(editingHistoryID);
             var layerID = WSM.INVALID_ID;
             // Get the existing layer ID.
@@ -56,14 +56,16 @@ FormIt.Commands.RegisterJSCommand("CutHoles.MakeCuttingObject");
 
 CutHoles.CutHoles = function()
 {
-    var currentHistory = FormIt.Model.GetHistoryID();
+    //console.log("------------------------------------- CutHoles.CutHoles -------------------------------------");
+    var currentHistory = FormIt.GroupEdit.GetEditingHistoryID();    
+    //console.log("currentHistory: " + JSON.stringify(currentHistory));
     var allHistorys = WSM.APIGetAllReachableHistoriesReadOnly(currentHistory, false);
-    console.log("allHistorys: " + JSON.stringify(allHistorys));
+    //console.log("allHistorys: " + JSON.stringify(allHistorys));
     var cuttingHistorys = [];
     for (var i = 0; i < allHistorys.length; ++i)
     {
         var cuttingHistory = allHistorys[i];
-        console.log("testing for cutting History: " + cuttingHistory);
+        //console.log("testing for cutting History: " + cuttingHistory);
         var attrib = WSM.Utils.GetStringAttributeForObject(cuttingHistory, WSM.INVALID_ID, "CutHoles.CutHole");
         //console.log(JSON.stringify(attrib));
         if(attrib.success)
@@ -71,27 +73,28 @@ CutHoles.CutHoles = function()
             cuttingHistorys.push(cuttingHistory);
         }
     }
-    console.log("cuttingHistorys: " + JSON.stringify(cuttingHistorys));
+    //console.log("cuttingHistorys: " + JSON.stringify(cuttingHistorys));
     var instances = [];
     for (var j = 0; j < cuttingHistorys.length; ++j)
     {
         var cuttingHistory = cuttingHistorys[j];
-        console.log("Getting Instances for History: " + cuttingHistory);
+        //console.log("Getting Instances for History: " + cuttingHistory);
         var instanceAggregate = WSM.APIGetAllAggregateTransf3dsReadOnly(cuttingHistory, currentHistory);
         instances.push.apply(instances, instanceAggregate.paths);
     }
-    console.log("instances: " + JSON.stringify(instances));
-    var allObjs = WSM.APIGetAllNonOwnedReadOnly(currentHistory);
+    //console.log("instances: " + JSON.stringify(instances));
     // Get the list of tools.
     var tools = [];
     for (var i = 0; i < instances.length; ++i)
     {
         var instance = instances[i];
-        console.log("instance: " + JSON.stringify(instance));
+        //console.log("instance: " + JSON.stringify(instance));
         var finalObjectHistoryID = WSM.GroupInstancePath.GetFinalObjectHistoryID(instance);
-        console.log("finalObjectHistoryID: " + JSON.stringify(finalObjectHistoryID));
+        //console.log("finalObjectHistoryID: " + JSON.stringify(finalObjectHistoryID));
         var nRefHistoryID = WSM.APIGetGroupReferencedHistoryReadOnly(finalObjectHistoryID.History, finalObjectHistoryID.Object);
+        //console.log("Tools History: " + nRefHistoryID);
         var allObjs = WSM.APIGetAllNonOwnedReadOnly(nRefHistoryID);
+        //console.log("All Tools allObjs: " + allObjs);
         for (var j = 0; j < allObjs.length; ++j)
         {
             var path = WSM.GroupInstancePath.AppendObjectHistoryID(instance, WSM.ObjectHistoryID(nRefHistoryID, allObjs[j]));
@@ -101,11 +104,13 @@ CutHoles.CutHoles = function()
             }
         }
     }
-    console.log("tools: " + JSON.stringify(tools));
-    for (var i = 0; i < allObjs.length; ++i)
+    //console.log("tools: " + JSON.stringify(tools));
+    var allBlankObjs = WSM.APIGetAllNonOwnedReadOnly(currentHistory);
+    //console.log("allBlankObjs: " + JSON.stringify(allBlankObjs));
+    for (var i = 0; i < allBlankObjs.length; ++i)
     {
-        var blank = WSM.ObjectHistoryID(currentHistory, allObjs[i]);
-        console.log("blank: " + JSON.stringify(blank));
+        var blank = WSM.ObjectHistoryID(currentHistory, allBlankObjs[i]);
+        //console.log("blank: " + JSON.stringify(blank));
         WSM.APISubtractNonDestructive(blank, tools);
     }
 }
